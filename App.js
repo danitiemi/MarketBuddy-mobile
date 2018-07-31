7a6f3099efa253aee0a896c4b48cc82b28a6b6c5
danitiemi <danitiemi.br@gmail.com>
2018-07-31 08:07:29 -0700

    Header and login raw UI

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavBar from 'market-buddy-mobile/app/components/Header';
+import Login from 'market-buddy-mobile/app/screens/Login';
+import { Button } from 'react-native-elements'
 
 export default class App extends React.Component {
+  
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text>Welcome to Market Buddy!!!</Text>
-        <Text>Save time, save MONEY!</Text>
+
+      <View style = { styles.container }>
+        <NavBar />
+        <View style = { styles.containerLarge }>
+          <View>
+            <Text>Save time, save MONEY!</Text>
+            {/* <Login /> */}
+            </View>
+            <Button
+              title = 'Login'
+              backgroundColor = '#5864bd'
+              // onPress={ this.onLogin.bind(this) }
+            />
+        </View>  
       </View>
+
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#330099'
+  },
+  containerLarge: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#330099'
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  }
 });
---
+++ app/screens/Login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Label } from 'react-native'
+import { Input, Button } from 'react-native-elements'
+// import Container from '../components/Container';
+// import Button from '../components/Button';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+    }
+});
+
+export default class Login extends Component {
+
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         username: '',
+    //         password: ''
+    //     };
+    // }
+
+    // onLogin() {
+    //     const { username, password } = this.state;
+    // }
+
+  render() {
+    
+    // return (
+    
+      
+    // );
+  }
+}
